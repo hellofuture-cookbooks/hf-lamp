efcad2f39c0af13c0a8882efaf739f6522bc8c9c
Andy Gale <andy@hellofutu.re>
2014-11-13 14:36:05 +0000

    Up version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'andy@hellofutu.re'
 license 'All rights reserved'
 description 'Installs/Configures phpapp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.16'
+version '0.1.17'
 
 depends 'apache2'
 depends 'mysql', '4.1.2'
