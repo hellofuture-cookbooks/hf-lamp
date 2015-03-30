196c50d72faa841a227c9dfa0aa9949b64c047be
Andy Gale <andy@hellofutu.re>
2015-03-30 14:21:36 +0100

    Bump version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'andy@hellofutu.re'
 license 'All rights reserved'
 description 'Installs/Configures phpapp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.17'
+version '0.1.18'
 
 depends 'apache2'
 depends 'mysql', '4.1.2'
